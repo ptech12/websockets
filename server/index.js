d8cf8faf75b4650051ab20584b9db132401b312d
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2023-12-23 13:53:39 +0530

    recieve from client and send uppercase data to client

--- server/index.js
+++ server/index.js
@@ -8,7 +8,15 @@ const wss = new WebSocket.Server({ port: 8080 });
 wss.on("connection", ws => {
     // on client connected
     console.log('Client Connected');
-
+    
+    // on client sending data
+    ws.on(
+        "message", data => {
+            console.log(`Client sent: ${data} `);
+            ws.send(data.toString().toUpperCase());
+            console.log(`Message send: ${data.toString().toUpperCase()}`);
+        }
+    )
 
 
     // on client disconnected
